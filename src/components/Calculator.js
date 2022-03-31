0e958cc1efb1f8f777eaa4ba66e62bcc0dac0c07
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-03-31 17:35:44 +0100

    Added Hooks functionality with use sate

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 const buttons = [
@@ -75,47 +75,40 @@ const operators = [
   },
 ];
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-    };
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null });
+  const { total, next } = state;
 
-    this.handleClick = this.handleClick.bind(this);
+  let result;
+  if (total === null && next !== null) {
+    result = next;
+  } else if (total !== null && next === null) {
+    result = total;
+  } else {
+    result = next;
   }
 
-  handleClick(e) {
-    this.setState((state) => (calculate(state, e.target.innerText)));
-  }
-
-  render() {
-    const { total, next } = this.state;
-    let result;
-    if (total === null && next !== null) {
-      result = next;
-    } else if (total !== null && next === null) {
-      result = total;
-    } else {
-      result = next;
-    }
-    return (
-      <div className="container">
-        <div className="header">
-          <div className="result">{result}</div>
+  const handleClick = (e) => {
+    const update = calculate(state, e.target.innerText);
+    setState(update);
+  };
+  return (
+    <div className="container">
+      <div className="resultContainer">
+        <div className="result">{result}</div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12 tab">
+          {buttons.map((button) => (<button key={button.name} onClick={handleClick} type="button" className="element">{button.value}</button>))}
+          <button type="button" name="0" className="element" onClick={handleClick}>0</button>
+          <button type="button" name="." className="element" onClick={handleClick}>.</button>
         </div>
-        <div className="row">
-          <div className="col-xs-12 tab">
-            {buttons.map((button) => (<button key={button.name} onClick={this.handleClick} type="button" className="element">{button.value}</button>))}
-            <button type="button" name="0" className="element" onClick={this.handleClick}>0</button>
-            <button type="button" name="." className="element" onClick={this.handleClick}>.</button>
-          </div>
-          <div className="operatorContainer">
-            {operators.map((button) => (<button key={button.name} onClick={this.handleClick} type="button" className="operator">{button.value}</button>))}
-          </div>
+        <div className="operatorContainer">
+          {operators.map((button) => (<button key={button.name} onClick={handleClick} type="button" className="operator">{button.value}</button>))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Calculator;
